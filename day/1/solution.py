8b9e1baeff6a947961f8730eba16396d20f6708c
Vince Tran <vietanhtran.96@gmail.com>
2021-09-28 11:12:28 -0500

    minor touch up for day 1

--- day/1/solution.py
+++ day/1/solution.py
@@ -1,33 +1,32 @@
-from typing import Union
+from typing import Iterable, Union
 
 
 S = 2020
 INPUT = "day/1/input"
 
-def findTwo(numbers: set, s: int) -> Union[int, None]:
+def findTwo(numbers: Iterable, s: int) -> Union[int, None]:
     for n in numbers:
         n_other = s - n
         if n_other in numbers:
             return n * n_other
     return None
 
-def part1():
-    with open(INPUT, "r") as openfile:
+def part1(input: str):
+    with open(input, "r") as openfile:
         numbers = set(int(line.strip("\n")) for line in openfile.readlines())
         return findTwo(numbers, S)
 
-def part2():
-    with open(INPUT, "r") as openfile:
+def part2(input: str):
+    with open(input, "r") as openfile:
         numbers = sorted(int(line.strip("\n")) for line in openfile.readlines())
         for i, n in enumerate(numbers):
             S_other = S - n
-            found = findTwo(set(numbers[i+1:]), S_other)
+            found = findTwo(numbers[i+1:], S_other)
             if found:
                 return n * found
         return None
 
 
-
 if __name__ == "__main__":
-    print(part1())
-    print(part2())
+    print(part1(INPUT))
+    print(part2(INPUT))
