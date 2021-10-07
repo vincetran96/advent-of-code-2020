9e17154d835b4d024f62d3e27cc4726dee9eefcb
Vince Tran <vince@vinMBP.local>
2021-10-06 20:34:14 -0500

    finish day 7

--- day/6/solution.py
+++ day/6/solution.py
@@ -18,29 +18,26 @@ def part1(input: str) -> int:
 
 def part2(input: str) -> int:
     with open(input, "r") as openfile:
-        group_questions = set()
+        group_questions = {""}
         total = 0
-        g = 0
         while True:
             line = openfile.readline()
             if not line or line == "\n":
-                if not group_questions:
+                if group_questions == {""}:
                     break
                 else:
-                    print(f'group {g}')
                     # print(group_questions)
+                    if "" in group_questions:
+                        group_questions.remove("")
                     total += len(group_questions)
-                group_questions = set()
-                g += 1
+                group_questions = {""}
             else:
-                if not group_questions:
-                    print(group_questions)
+                if group_questions == {""}:
                     group_questions.update(set(line.strip()))
                 else:
-                    print(group_questions.intersection(set(line.strip())))
                     group_questions = group_questions.intersection(set(line.strip()))
         return total
 
 if __name__ == "__main__":
-    # print(part1(INPUT))
+    print(part1(INPUT))
     print(part2(INPUT))
--- day/7/solution.py
+++ day/7/solution.py
@@ -17,6 +17,23 @@ def extract_child_parent(s: str) -> tuple:
             c_colors_tup = (*c_colors_tup, re.split(numbas, c)[1])
     return p_color, c_colors_tup
 
+def extract_parent_numchild(s: str) -> tuple:
+    '''Extracts parent, children and number of each child
+    '''
+    bags = r" *bags*"
+    numbas = r"\d+ +"
+
+    p, c = s.split("contain")
+    p_color = re.split(bags, p.strip())[0].strip()
+    c_colors = re.split(bags, c.strip())
+    child_num = ()
+    for c in c_colors:
+        numba = re.search(numbas, c)
+        if numba:
+            child_num = (
+                *child_num, (int(numba[0].strip()), re.split(numbas, c)[1].strip()))
+    return p_color, child_num
+
 def build_chash(iter: Iterable):
     '''Builds a hash with children as keys,
     tuples of parents as values
@@ -33,6 +50,19 @@ def build_chash(iter: Iterable):
             result[parent] = set()
     return result
 
+def build_phash(iter: Iterable):
+    '''Builds a hash with parents as keys,
+    children and its numbers as values
+    '''
+    result = {}
+    for line in iter:
+        parent, num_child = extract_parent_numchild(line)
+        if parent not in result:
+            result[parent] = {t[1]: t[0] for t in num_child}
+        else:
+            result[parent] = {**result[parent], **{t[1]: t[0] for t in num_child}}
+    return result
+
 def find_parents(h: dict, child: str, found: set = set()):
     '''Finds the set of parents for the `child` in hash `h`
     '''
@@ -45,10 +75,27 @@ def find_parents(h: dict, child: str, found: set = set()):
     else:
         return set()
 
-def solve(input: str):
+def find_num_children(h: dict, parent: str):
+    '''Finds the total number of bags for `parent` in hash `h`
+    '''
+    children = h[parent]
+    if children:
+        return sum(
+            num + num * find_num_children(h, color) for color, num in children.items()
+        )
+    else:
+        return 0
+
+def part1(input: str):
     with open(input, "r") as openfile:
         h = build_chash(openfile.readlines())
         return len(find_parents(h, "shiny gold"))
 
+def part2(input: str):
+    with open(input ,"r") as openfile:
+        h = build_phash(openfile.readlines())
+        return find_num_children(h, "shiny gold")
+
 if __name__ == "__main__":
-    print(solve(INPUT))
+    print(part1(INPUT))
+    print(part2(INPUT))
