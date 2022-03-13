e3d6607677bce13e6a42d4b6111fc70494470921
Vince Tran <vietanhtran.96@gmail.com>
2022-03-13 10:48:20 -0500

    stuck in day 16 part 2

--- day/16/solution.py
+++ day/16/solution.py
@@ -13,9 +13,12 @@ def check_condition(
     return (input >= cond[0] and input <= cond[1]) \
             or (input >= cond[2] and input <= cond[3])
 
-def part1(input: str) -> int:
-    conditions = {}
+def part1(input: str) -> tuple:
+    conditions = {} # Dict of conditions names and conditions values
+    conditions_order = {} # Dict of correct order of conditions in each ticket line
+    valid_tickets = [] # List of valid ticket values
     nearby = False
+    your = False
     result = 0
     with open(input, "r") as openfile:
         while True:
@@ -32,23 +35,69 @@ def part1(input: str) -> int:
                     for value in e.split("-"):
                         condition = condition + (int(value), )
                 conditions[field] = condition
+
+            # Lines with "your" ticket info
+            if "your" in line and your is False:
+                your = True
+            if your and "," in line:
+                your_ticket_values = [int(n) for n in line.strip().split(",")]
             
             # Lines with nearby tickets info
             if "nearby" in line and nearby is False:
                 nearby = True
+                your = False
             if nearby and "," in line:    
                 ticket_values = [int(n) for n in line.strip().split(",")]
+                isValid_ticket = True # Validity of the ticket
                 for ticket_value in ticket_values:
-                    valid = False
-                    for condition in conditions.values():
-                        if check_condition(ticket_value, condition):
-                            valid = True
+                    isValid_value = False
+                    for ck, cv in conditions.items():
+                        if check_condition(ticket_value, cv):
+                            isValid_value = True
+                            
+                            # # Append condition name to cond. list if not in
+                            # if ck not in conditions_list:
+                            #     conditions_list.append(ck)
+                            
+                            # # Skip checking altogether if cond. list is full
+                            # if len(conditions_list) == len(conditions.keys()):
+                            #     break
+                            
                             break
-                    if valid is False:
+                    # If one ticket value is invalid, the ticket is invalid
+                    if isValid_value is False:
                         result += ticket_value
-    return result
+                        isValid_ticket = False
+                if isValid_ticket is True:
+                    valid_tickets.append(ticket_values)
+                # something is wrong with the valid tickets list
+
+
+        
+        # Part 2?
+        unchecked_idx = sorted(list(range(len(conditions.keys()))))
+        print(unchecked_idx)
+        for ck, cv in conditions.items():
+            print(f"Checking condition {ck}")
+            for idx in unchecked_idx:
+                isCorrect_idx = True
+                for ticket_values in valid_tickets:
+                    if check_condition(ticket_values[idx], cv) is False:
+                        isCorrect_idx = False
+                        break
+                if isCorrect_idx is True:
+                    print(f">> Index {idx} is correct for condition {ck}")
+                    conditions_order[ck] = idx
+                    unchecked_idx.remove(idx) # Not sure if this list is updated within for loop
+                    break
+            print(f">> {unchecked_idx}")
+
+
+                    
+    return result, conditions_order
 
 
 if __name__ == "__main__":
-    print(part1(INPUT))
+    print(part1(INPUT_TEST))
+    # print(part1(INPUT))
     # print(check_condition(35, (30, 260, 284, 950)))
